2914b6d18e70088ae27a438692c8a441c9a198eb
Om <ramanacomfy@gmail.com>
2022-02-24 12:02:59 -0800

    filter books by specific author

--- filter/views.py
+++ filter/views.py
@@ -4,10 +4,14 @@ from rest_framework import viewsets
 from filter.models import Book
 from filter.models import Author
 from filter.serializers import BookSerializer,AutherSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class BookViewset(viewsets.ModelViewSet):
    queryset = Book.objects.all()
    serializer_class = BookSerializer
+   filter_backends = [DjangoFilterBackend]
+   filterset_fields = ['author', 'title']
+
 
 class AuthorViewset(viewsets.ModelViewSet):
    queryset = Author.objects.all()
